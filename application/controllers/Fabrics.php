c282f6c8c611920e31f4983ece6d30b43ffc2872
Ross Mcinerney <62793400+RossMci@users.noreply.github.com>
2021-01-11 19:57:53 +0000

    thumbnails working

--- application/controllers/Fabrics.php
+++ application/controllers/Fabrics.php
@@ -2,47 +2,53 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Fabrics extends CI_Controller {
+class Fabrics extends CI_Controller
+{
 
-    public function index() {
+
+    
+
+    public function index()
+    {
         $this->load->model('FabricRepository');
         $fabrics = $this->FabricRepository->getFabrics();
 
         $errors = "";
         $displayBlock = "";
-        $displayBlock2 ="";
-      
+        $displayBlock2 = "";
+
         foreach ($fabrics as $fabric) {
+
+            $extension = strrchr($fabric->image , '.');
+            $name = substr($fabric->image , 0, -strlen($extension));
+            $thumbnailPath = $name."_Thumb".$extension;
 
             $imageConfig = array(
                 "image_library" => 'GD2', //image library
-                "source_image" => '' . $fabric->image,
-                "new_image" => 'assets/images/fabrics/thumbnail/' . basename($fabric->image),
+                "source_image" => './' . $fabric->image,
+                //"new_image" => './assets/images/fabrics/thumbnail/',
                 "create_thumb" => TRUE,
                 "maintain_ratio" => TRUE,
                 "width" => 150,
-                "height" => 100
+                "height" => 150
             );
 
 
             $this->load->library('image_lib', $imageConfig);
             $this->image_lib->initialize($imageConfig);
 
-            $imageFilePath = $imageConfig["new_image"];
             if (!$this->image_lib->resize()) {
-                $errors .= "<p>" . $imageConfig["new_image"] . "</p>\n";
                 $errors .= $this->image_lib->display_errors();
-//                $imageFilePath=$imageConfig["source_image"];
             }
 
-                $displayBlock .= "<td><img  src=" . base_url() . $imageFilePath . "_thumb.jpg></td>\n"; //.$errors;
-                  $displayBlock2 .= "<td>" . $fabric->name . "</td>\n";
+            $displayBlock .= '<td><img  src="' . base_url() . $thumbnailPath . '"></td>' . $errors;
+            $displayBlock2 .= "<td>" . $fabric->name . "</td>\n";
         }
-   
+
 
         $data = array(
             'displayBlock' => $displayBlock,
-            'displayBlock2'=>$displayBlock2
+            'displayBlock2' => $displayBlock2
         );
 
         $contentData = array(
@@ -51,7 +57,6 @@ class Fabrics extends CI_Controller {
         $this->image_lib->clear();
         $this->load->view('fabrics', $contentData);
     }
-
 }
 
 /* End of file  Frabrics.php */
--- application/views/content/fabrics_content.php
+++ application/views/content/fabrics_content.php
@@ -11,8 +11,8 @@ $base_url = base_url();
             knitting, crocheting, knotting,
             tatting, felting, bonding or braiding these yarns together.
             The related words "fabric"[3] and "cloth"[4] and "material" are often used in textile assembly trades (such as tailoring and dressmaking)
-            as synonyms for textile. However, there are subtle differences in these terms in specialized usage. A textile is any material made of the interlacing fibres, 
-            including carpeting and geotextiles, which may not necessarily be used in the production of further goods, such as clothing and upholstery. 
+            as synonyms for textile. However, there are subtle differences in these terms in specialized usage. A textile is any material made of the interlacing fibres,
+            including carpeting and geotextiles, which may not necessarily be used in the production of further goods, such as clothing and upholstery.
             A fabric is a material made through weaving, knitting, spreading, felting, stitching, crocheting or bonding that may be used in the production of further products,
 
 
@@ -31,27 +31,15 @@ $base_url = base_url();
         </form>
         <table>
             <tr>
-            <th>d</th>
-            <th>d</th>
+                <th>a</th>
+                <th>b</th>
+                <th>c</th>
             </tr>
             <tr>
-            <?php
-//	  foreach($fabrics as $fabric)
-//	  {
-//                     echo "<br/>";
-////                   echo '<img src = "' . $this->config->item("image_url") . 'courses/null_course.jpg" alt ="'.$course['title'].'">';
-//                   echo '"<img src=" . base_url() . "images/" . $row->item_image . ">"';
-//                     echo "<br/>";
-//		  echo "<br/>";
-//
-//	  }
-            
-            echo $displayBlock;
-            ?>
+                <?php echo $displayBlock; ?>
             </tr>
-            
             <tr>
-                <?php   echo $displayBlock2; ?>
+                <?php echo $displayBlock2; ?>
             </tr>
         </table>
     </div>
